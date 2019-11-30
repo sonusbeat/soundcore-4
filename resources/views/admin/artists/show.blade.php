cccab680d3124d693e3d79876a12639b89cd984d
Daniel Gonzalez Briseno <sonusbeat@hotmail.com>
2019-11-30 16:47:37 -0500

    Singles bug fixed in show

--- resources/views/admin/artists/show.blade.php
+++ resources/views/admin/artists/show.blade.php
@@ -105,7 +105,7 @@
             </table>
         </div><!-- /.col -->
     </div><!-- /.row -->
-    @if($artist->singles != [])
+    @if(count($artist->singles))
     <div class="row ml-4 mb-4">
         <div class="col-md-6">
             <h2>Songs</h2>
